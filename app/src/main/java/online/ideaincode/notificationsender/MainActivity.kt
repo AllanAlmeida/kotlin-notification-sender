9f76d205fa9746f3b98c2956d78633d6c59617d1
almeida.allan <allan.almeida.cpmb@gmail.com>
2024-06-04 09:38:11 -0300

    Adjusting request permission flow.

--- app/src/main/java/online/ideaincode/notificationsender/MainActivity.kt
+++ app/src/main/java/online/ideaincode/notificationsender/MainActivity.kt
@@ -6,15 +6,23 @@ import android.app.NotificationManager
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+const val POST_NOTIFICATION_PERMISSION = Manifest.permission.POST_NOTIFICATIONS
+const val POST_NOTIFY_REQ_CODE = 100
 const val CHANNEL_ID = "primary_channel_id"
 
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
 class MainActivity : AppCompatActivity() {
 
     private val simpleNotification: Button by lazy { findViewById(R.id.simpleNotification) }
@@ -25,13 +33,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         createNotificationChannel()
+        requestPostNotificationPermission()
+    }
 
+    private fun notificator() {
         simpleNotification.setOnClickListener {
-            sendNotification(1, "Simple Notification", "This is a test of simple notification", "")
+            sendNotification(
+                1,
+                "Simple Notification",
+                "This is a test of simple notification",
+                ""
+            )
         }
 
         longNotification.setOnClickListener {
-            sendNotification(2, "Long Notification", "", "This is a test of long notification: Lorem ipsum dolor sit amet. Qui dolorem saepe At dolorum deserunt ab harum quos ut laudantium galisum et cupiditate voluptas At quae consequatur. Sed odit deserunt est obcaecati unde eum minus libero quo autem voluptatem non quas quisquam ut minus omnis.")
+            sendNotification(
+                2,
+                "Long Notification",
+                "",
+                "This is a test of long notification: Lorem ipsum dolor sit amet. " +
+                        "Qui dolorem saepe At dolorum deserunt ab harum quos ut laudantium galisum " +
+                        "et cupiditate voluptas At quae consequatur. Sed odit deserunt est obcaecati " +
+                        "unde eum minus libero quo autem voluptatem non quas quisquam ut minus omnis."
+            )
         }
     }
 
@@ -45,9 +69,69 @@ class MainActivity : AppCompatActivity() {
                 description = descriptionText
             }
             val notificationManager: NotificationManager =
-                ContextCompat.getSystemService(this, NotificationManager::class.java) as NotificationManager
+                ContextCompat.getSystemService(
+                    this,
+                    NotificationManager::class.java
+                ) as NotificationManager
 
             notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun requestPostNotificationPermission() = when {
+        ContextCompat.checkSelfPermission(
+            this,
+            POST_NOTIFICATION_PERMISSION
+        ) == PackageManager.PERMISSION_GRANTED -> {
+            Log.i("[DEBUG]","GRANTED")
+            Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_LONG).show()
+            notificator()
+        }
+
+        ActivityCompat.shouldShowRequestPermissionRationale(
+            this,
+            POST_NOTIFICATION_PERMISSION
+        ) -> {
+            Log.i("[DEBUG]","-----------------------------> NOT GRANTED")
+            Toast.makeText(this, "Permission NOT GRANTED", Toast.LENGTH_LONG).show()
+            requestPermissions(
+                arrayOf(POST_NOTIFICATION_PERMISSION),
+                POST_NOTIFY_REQ_CODE
+            )
+        }
+
+        else -> {
+            Log.i("[DEBUG]","-----------------------------> ELSE")
+            requestPermissions(
+                arrayOf(POST_NOTIFICATION_PERMISSION),
+                POST_NOTIFY_REQ_CODE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        Log.i("[DEBUG]","-----------------------------> REQUEST CODE: $requestCode")
+
+        when (requestCode) {
+            POST_NOTIFY_REQ_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    notificator()
+                } else {
+                    doDialog(
+                        "Please, don't make me beg",
+                        "We are a notifications app, if you cannot grant this permission we " +
+                                "will not work properly. So, go to the settings and give us these " +
+                                "permissions, ok?!"
+                    )
+                }
+                return
+            }
         }
     }
 
@@ -58,7 +142,6 @@ class MainActivity : AppCompatActivity() {
         if (longText.isNotEmpty()) {
             longNotificationText = NotificationCompat.BigTextStyle().bigText(longText)
         }
-
 
         val builder = NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(R.mipmap.ic_launcher)
@@ -77,5 +160,20 @@ class MainActivity : AppCompatActivity() {
             }
             notify(id, builder.build())
         }
+    }
+
+    private fun doDialog(title: String, message: String) {
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("OK") { dialogInterface, _ ->
+                dialogInterface.dismiss()
+            } // Enable code bellow if you want to show cancel button
+//            .setNegativeButton(
+//                "CANCEL"
+//            ) {
+//              dialogInterface, _ -> dialogInterface.dismiss()
+//            }
+            .show()
     }
 }
